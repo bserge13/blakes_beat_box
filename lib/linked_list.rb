243217b4278029d110545c9b1397363a2ea03204
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-12 16:05:57 -0500

    add the methods append, next_node, count, and to_string to linked_list class

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,24 @@
 class LinkedList
+    attr_reader :head, 
+                :data,
+                :append, 
+                :next_node, 
+                :count, 
+                :to_string
     def initialize
+        @head = nil
+        @data = [data]
+    end
+    def append(sound)
+        @data << sound 
+    end
+    def next_node
+        # code to be written
+    end
+    def count
+        @data.count 
+    end
+    def to_string
+        # code to be written
+    end
 end

51390a40884b557c9b86751fa9c530b42f063ca4
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-13 14:15:28 -0500

    write initial test for append, next_node, count, and to_string

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -3,7 +3,6 @@ require './lib/node'
 RSpec.describe Node do 
     it 'exists' do 
         node = Node.new("plop")
-        expect(node).to be_instance_of(Node)
     end
     it 'holds data' do
         node = Node.new("plop")

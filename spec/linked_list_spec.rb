a34dbd5e73364f090337102657e91a8adddcaf21
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-13 14:20:07 -0500

    add initial test for append, next_node, count, to_string methods

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,13 +1,28 @@
-require '.lib/linked_list'
-require '.lib/node'
+require './lib/linked_list'
+require './lib/node'
 
 RSpec.describe LinkedList do 
     it 'exists' do 
         list = LinkedList.new 
-        expect(list).to be_instance_of(LinkedList)
     end
     it 'checks the status of the head' do 
         list = LinkedList.new 
         expect(list.head).to eq(nil)
     end
+    it 'adds appends data' do
+        list = LinkedList.new
+        expect(list.append("doop")).to eq(["doop"])
+    end 
+    it 'checks for the next node from the head' do
+        list = LinkedList.new 
+        expect(linked_list.head.next_node).to eq(nil)
+    end 
+    it 'counts number of nodes in list' do
+        list = LinkedList.new
+        expect(list.count).t eq(1)
+    end
+    it 'returns the data of node in the list as a string' do
+        list = LinkedList.new
+        expect(list.to_string).to eq("doop")
+    end 
 end 

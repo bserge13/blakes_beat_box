daedf98dd04d153f0b603f8f0c759d2e5e123953
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-14 21:30:22 -0500

    add code to count, prepend, append, and is_empty? methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,25 +1,60 @@
 class LinkedList
     attr_reader :head, 
                 :data,
-                :append, 
                 :next_node, 
-                :count, 
-                :to_string
     def initialize
-        @head = nil
+        @head = head 
         @data = []
-        @next_node = next_node 
+        @next = next_node
+    end
+    def is_empty? 
+        if @head == nil
+            true
+        else 
+            false
+        end
     end
     def append(data)
-        @data << data 
+        if list.is_empty?
+            @head = Node.new(data)
+        else 
+            current_node = @head
+            new_node = Node.new(data)
+            while current_node.next != nil
+                current_node = current_node.next 
+            end
+            current_node.next = new_node
+        end
     end
-    # def next_node
-    #     # code to be written
-    # end
+    def prepend(data)
+        if list.is_empty?
+            @head = Node.new(data)
+        else
+            current_node = Node.new(data)
+            current_node.next = @head
+            @head = current_node
+        end
+    end 
+    def insert
+        # code to be written for inserting node 
+        # at specific position/index. 
+    end
+    def next_node
+        next_node
+    end
     def count
-        @data.count 
+        if list.is_empty?
+            count = 0
+        else 
+            count = 1
+            current_node = @head 
+            while current_node.next != nil
+                current_node = current_node.next
+                count += 1
+            end 
+        end
     end
     def to_string
-        # code to be written
+        list.data.to_s
     end
 end

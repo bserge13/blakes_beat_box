0065b3d3e0e6bb37b1ae72a90fc0d8ca994df04d
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-14 21:28:03 -0500

    alter spec test file

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -4,24 +4,25 @@ require './lib/node'
 RSpec.describe LinkedList do 
     it 'exists' do 
         list = LinkedList.new 
+        node = Node.new("doop")
     end
     it 'checks the status of the head' do 
         list = LinkedList.new 
         expect(list.head).to eq(nil)
     end
-    it 'adds appends data' do
+    it 'appends new node of head of list' do
         list = LinkedList.new
-        expect(list.append("doop")).to eq(["doop"])
+        expect(list.append("doop")).to eq("doop")
     end 
-    it 'checks for the next node from the head' do
+    it 'checks for next node from the head' do
         list = LinkedList.new 
-        expect(linked_list.head.next_node).to eq(nil)
+        expect(list.head.next_node).to eq(nil)
     end 
-    it 'counts number of nodes in list' do
+    it 'counts number of data in list' do
         list = LinkedList.new
-        expect(list.count).t eq(1)
+        expect(list.count).to eq(1)
     end
-    it 'returns the data of node in the list as a string' do
+    it 'returns the lists data as a string' do
         list = LinkedList.new
         expect(list.to_string).to eq("doop")
     end 

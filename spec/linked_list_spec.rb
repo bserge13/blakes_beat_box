aefb55f9e9511f5ca048583346b28dc2e35ab0a1
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-16 18:27:24 -0500

    reword testing expectations, and appends tes

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,29 +1,36 @@
 require './lib/linked_list'
 require './lib/node'
+require 'pry'
 
 RSpec.describe LinkedList do 
     it 'exists' do 
         list = LinkedList.new 
-        node = Node.new("doop")
     end
     it 'checks the status of the head' do 
         list = LinkedList.new 
+
         expect(list.head).to eq(nil)
     end
-    it 'appends new node of head of list' do
+    it 'appends node to list' do
         list = LinkedList.new
-        expect(list.append("doop")).to eq("doop")
+
+        list.append("doop")
+        expect(list.append("doop").to eq("doop"))
     end 
-    it 'checks for next node from the head' do
+    it 'checks for next node from head' do
         list = LinkedList.new 
+
         expect(list.head.next_node).to eq(nil)
     end 
-    it 'counts number of data in list' do
+    it 'counts number of nodes in list' do
         list = LinkedList.new
+        node = node.new("doop")
+
         expect(list.count).to eq(1)
     end
     it 'returns the lists data as a string' do
         list = LinkedList.new
+
         expect(list.to_string).to eq("doop")
     end 
 end 
